b415cdd04c20e0422d1db418523bf6445e6031c9
Samuel Porras <samuelporras@gmail.com>
2019-02-07 22:42:57 +0100

    First version work component and added moment.js

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,12 +1,52 @@
 <template>
-
+    <div>
+        <h3>{{ category }}</h3>
+        <i>{{ business }}</i>
+        <div>
+            <span>{{ initialDateFormatted }}</span> - <span>{{ finalDateFormatted }}</span> · <i> {{ timeLapseFormatted }} </i>
+        </div>
+        <div v-html="functions"></div>
+    </div>
 </template>
 
 
 <script>
+const moment = require('moment');
+
+function formatDateTime(date) {
+    let result = moment(date, ["DD-MM-YYYY"], "es").format("MMMM-YYYY");
+    console.log(result);
+    return result;    
+}
+
+function timeLapse(datefrom, dateto) {
+    let dateFrom = moment(datefrom, ["DD-MM-YYYY"], "es");
+    let dateTo = moment(dateto, ["DD-MM-YYYY"], "es");
+    
+    return dateTo.from(dateFrom, true)
+}
+
 // @ is an alias to /src
 
 export default {
+    props: [
+        "business",
+        "initialDate",
+        "finalDate",
+        "category", 
+        "functions"
+        ],
+        computed: {
+            initialDateFormatted: function() {
+                return formatDateTime(this.initialDate);
+            },
+            finalDateFormatted: function() {
+                return formatDateTime(this.finalDate)
+            },
+            timeLapseFormatted: function() {
+                return timeLapse(this.initialDate, this.finalDate);
+            }
+        }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="home">
+    <h2>Experiencia Laboral</h2>
+    <Work 
+    v-for="(work ,key) in works" 
+    :key="key" 
+    :business="work.business"
+    :initialDate="work.initial_date"
+    :finalDate="work.final_date"
+    :category="work.category"
+    :functions="work.functions"></Work>
   </div>
 </template>
 
 <script>
+const axios = require('axios');
+import Work from '@/components/Work.vue';
 // @ is an alias to /src
 
 export default {
+  data: function() {
+    return {
+      works: []
+    }
+  },
+  mounted() {
+    axios.get('https://curriculum-6563e.firebaseio.com/experience.json').then( response => {
+      this.works = response.data;
+    }).catch( error => {
+      console.log(error);
+    })
+  },
+  components: {
+    Work
+  }
 }
 </script>
