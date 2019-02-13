57f50d43b7aedb23affd5a37f0ff3a43412780a9
Samuel Porras <Arkiant@users.noreply.github.com>
2019-02-13 13:40:33 +0100

    Update conversor.js

--- src/libs/conversor.js
+++ src/libs/conversor.js
@@ -1,5 +1,7 @@
 /**
  * Converts a object into an array
+ * @params {Object} object
+ * @returns {Array}
  */
 function convertObjectToArray(object) {
     return Object.entries(object).map(e => Object.assign(e[1], {key: e[0]}));
