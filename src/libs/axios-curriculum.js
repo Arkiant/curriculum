d6cc3ee133e855cd604627a8568cc5ac802f1e7c
Samuel Porras <samuelporras@gmail.com>
2020-09-08 12:06:27 +0200

    Refactor: changed firebase to json files

--- src/libs/axios-curriculum.js
+++ src/libs/axios-curriculum.js
@@ -1,7 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
-const instance = axios.create({
-    baseURL: 'https://curriculum-6563e.firebaseio.com'
-})
+const instance = axios.create({});
 
-export default instance
+export default instance;
--- src/store/curriculum.js
+++ src/store/curriculum.js
@@ -1,54 +1,56 @@
 import axios from "@/libs/axios-curriculum.js";
-import { sortByDate } from "@/libs/time.js";
 import { convertObjectToArray } from "@/libs/conversor.js";
+import { sortByDate } from "@/libs/time.js";
 
 const state = {
-    works: [],
-    profile: {}
-}
+  works: [],
+  profile: {},
+};
 
 const mutations = {
-    'INIT_WORKS' (state) {
-        axios.get('/experience.json')
-        .then( response => {
-            state.works = convertObjectToArray(response.data).sort(sortByDate);
-        })
-        .catch( error => {
-            console.log(error);
-        })
-    },
-    'INIT_PROFILE' (state) {
-        axios.get('/profile.json?orderBy="$key"&startAt="-LZaUFYjlEgKkK1ywJHD"&endAt="-LZaUFYjlEgKkK1ywJHD"')
-        .then( response => {
-            state.profile = response.data["-LZaUFYjlEgKkK1ywJHD"];
-        })
-        .catch( error => {
-            console.log(error);
-        })
-    }
-}
+  INIT_WORKS(state) {
+    axios
+      .get("/data/experiences.json")
+      .then((response) => {
+        state.works = convertObjectToArray(response.data).sort(sortByDate);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  INIT_PROFILE(state) {
+    axios
+      .get("/data/profile.json")
+      .then((response) => {
+        state.profile = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
 
 const actions = {
-    initWorks: ({commit}) => {
-        commit('INIT_WORKS')
-    },
-    initProfile: ({commit}) => {
-        commit('INIT_PROFILE')
-    }
-}
+  initWorks: ({ commit }) => {
+    commit("INIT_WORKS");
+  },
+  initProfile: ({ commit }) => {
+    commit("INIT_PROFILE");
+  },
+};
 
 const getters = {
-    getWorks: state => {
-        return state.works;
-    },
-    getProfile: state => {
-        return state.profile;
-    }
-}
+  getWorks: (state) => {
+    return state.works;
+  },
+  getProfile: (state) => {
+    return state.profile;
+  },
+};
 
 export default {
-    state,
-    mutations,
-    actions,
-    getters
-}
+  state,
+  mutations,
+  actions,
+  getters,
+};
